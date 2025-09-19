16d7c0bd9e4b156ed69276d8de7ddc2e932ce865
Emerson Lima <ecorreiaflima@gmail.com>
2025-09-19 12:40:59 -0300

    Path

--- src/Path.php
+++ src/Path.php
@@ -2,7 +2,7 @@
 
 namespace Ecfl\Webpro;
 
-#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_FUNCTION | Attribute::TARGET_CLASS)]
+#[\Attribute(\Attribute::TARGET_METHOD | \Attribute::TARGET_FUNCTION | \Attribute::TARGET_CLASS)]
 class Path {
 	private $path;
 
